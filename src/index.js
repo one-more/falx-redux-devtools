6ac8b53f9b18d2203fa2892e6971254ddd353694
dmitrii_nikolaev <greyjoi@yandex.ru>
2018-02-11 14:14:07 +0300

    source code

---
+++ src/index.js
@@ -0,0 +1,133 @@
+import {use} from 'falx'
+
+function createStore(reducer, preloadedState, enhancer) {
+    if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
+        enhancer = preloadedState;
+        preloadedState = undefined
+    }
+
+    if (typeof enhancer !== 'undefined') {
+        if (typeof enhancer !== 'function') {
+            throw new Error('Expected the enhancer to be a function.')
+        }
+
+        return enhancer(createStore)(reducer, preloadedState)
+    }
+
+    if (typeof reducer !== 'function') {
+        throw new Error('Expected the reducer to be a function.')
+    }
+
+    let currentReducer = reducer;
+    let currentState = preloadedState;
+    let currentListeners = [];
+    let nextListeners = currentListeners;
+    let isDispatching = false;
+
+    function ensureCanMutateNextListeners() {
+        if (nextListeners === currentListeners) {
+            nextListeners = currentListeners.slice()
+        }
+    }
+
+    function getState() {
+        if (isDispatching) {
+            throw new Error(
+                'You may not call store.getState() while the reducer is executing. ' +
+                'The reducer has already received the state as an argument. ' +
+                'Pass it down from the top reducer instead of reading it from the store.'
+            )
+        }
+
+        return currentState
+    }
+
+    function subscribe(listener) {
+        if (typeof listener !== 'function') {
+            throw new Error('Expected the listener to be a function.')
+        }
+
+        if (isDispatching) {
+            throw new Error(
+                'You may not call store.subscribe() while the reducer is executing. ' +
+                'If you would like to be notified after the store has been updated, subscribe from a ' +
+                'component and invoke store.getState() in the callback to access the latest state. ' +
+                'See http://redux.js.org/docs/api/Store.html#subscribe for more details.'
+            )
+        }
+
+        let isSubscribed = true
+
+        ensureCanMutateNextListeners()
+        nextListeners.push(listener)
+
+        return function unsubscribe() {
+            if (!isSubscribed) {
+                return
+            }
+
+            if (isDispatching) {
+                throw new Error(
+                    'You may not unsubscribe from a store listener while the reducer is executing. ' +
+                    'See http://redux.js.org/docs/api/Store.html#subscribe for more details.'
+                )
+            }
+
+            isSubscribed = false
+
+            ensureCanMutateNextListeners()
+            const index = nextListeners.indexOf(listener)
+            nextListeners.splice(index, 1)
+        }
+    }
+
+    function dispatch(action) {
+        if (typeof action.type === 'undefined') {
+            throw new Error(
+                'Actions may not have an undefined "type" property. ' +
+                'Have you misspelled a constant?'
+            )
+        }
+
+        if (isDispatching) {
+            throw new Error('Reducers may not dispatch actions.')
+        }
+
+        try {
+            isDispatching = true
+            currentState = currentReducer(currentState, action)
+        } finally {
+            isDispatching = false
+        }
+
+        const listeners = (currentListeners = nextListeners)
+        for (let i = 0; i < listeners.length; i++) {
+            const listener = listeners[i]
+            listener()
+        }
+
+        return action
+    }
+
+    dispatch({ type: '@@redux/INIT' });
+
+    return {
+        dispatch,
+        subscribe,
+        getState
+    }
+}
+
+export function connectDevtools(devtoolsExtension) {
+    const $store = createStore(
+        store.getState,
+        devtoolsExtension()
+    );
+
+    use(function(store, promise, action) {
+        return promise.then(state => {
+            $store.dispatch(action);
+            return state
+        })
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'main.js',
+        library: 'falx',
+        libraryTarget:'umd'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    externals: {
+        falx: {
+            commonjs: 'falx',
+            commonjs2: 'falx',
+            amd: 'falx',
+            root: 'falx'
+        }
+    }
+};
